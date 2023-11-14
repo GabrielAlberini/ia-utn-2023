9b5e0571dbd44d2b9e7e3f6d13ffb37cd52775f0
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-13 23:35:08 -0300

    Arreglo de router en pizza-app-frontend

--- pizza-app-utn/pizza-app-frontend/src/router.jsx
+++ pizza-app-utn/pizza-app-frontend/src/router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Users } from "../components/Users/Users";
-import { List } from "../components/List/List";
-import { ViewNotFound } from "../components/ViewNotFound/ViewNotFound";
+import { Users } from "./components/Users/Users";
+import { List } from "./components/List/List";
+import { ViewNotFound } from "./components/ViewNotFound/ViewNotFound";
 
 export const Router = () => {
   return (
